b21078353cf4dbef24608a532995f73d2e4603cc
sereja <sergejerofeev66@gmail.com>
2023-11-21 11:52:39 +0300

    example_005

---
+++ example005_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("ура это Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
